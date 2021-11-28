40c297db3d843a8364348ceec9d2ef9c3e48b5c2
Matheus Vieira <mvdias20@gmail.com>
2021-11-28 11:27:15 -0300

    Config media queries [4] / toggle menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function imgSlider(any) {
     document.querySelector('.jacket').src = any;
 }
+
+
+const menutoggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+menutoggle.onclick = function(){
+    menutoggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
